bbbefbca2ae483177639ede288763cdd04d13059
Sergiy Yurov <urovsergej10@gmail.com>
2025-02-01 16:55:26 +0200

    add all task views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "todo_list.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = ["__all__"]
+    success_url = reverse_lazy("todo_list.html")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = ["__all__"]
+    success_url = reverse_lazy("todo_list.html")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    fields = ["__all__"]
+    success_url = reverse_lazy("todo_list.html")
