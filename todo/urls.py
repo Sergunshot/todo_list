4d57748255b1790d0c474431700ed8a70982b646
Sergiy Yurov <urovsergej10@gmail.com>
2025-02-03 15:03:54 +0200

    add TaskSetStatusView

--- todo/urls.py
+++ todo/urls.py
@@ -9,7 +9,7 @@ from todo.views import (
     TagCreateView,
     TagUpdateView,
     TagDeleteView,
-    change_status
+    TaskSetStatusView
 )
 
 urlpatterns = [
@@ -17,7 +17,7 @@ urlpatterns = [
     path("create/", TaskCreateView.as_view(), name="task-create"),
     path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("<int:pk>/change-status", change_status, name="task-change-status"),
+    path("<int:pk>/change-status", TaskSetStatusView.as_view(), name="task-change-status"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
