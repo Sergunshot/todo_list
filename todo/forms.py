df83ad3d7eb697c0c2e54693e409b48b4b2a3a08
Sergiy Yurov <urovsergej10@gmail.com>
2025-02-03 15:02:15 +0200

    rewrite TodoForm on TaskCreationForm

--- todo/forms.py
+++ todo/forms.py
@@ -1,12 +1,48 @@
+import datetime
+
 from django import forms
+from django.core.exceptions import ValidationError
 
 from todo.models import Task, Tag
 
 
-class TodoForm(forms.ModelForm):
+def validate_deadline(deadline):
+    if deadline and deadline < datetime.date.today():
+        raise ValidationError(
+            f"Deadline date should be not less than today: "
+            f"{datetime.date.today().strftime('%d-%m-%Y')}"
+        )
+
+    return deadline
+
+
+class TaskCreationForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    expected_deadline_date = datetime.date.today() + datetime.timedelta(days=5)
+    deadline = forms.DateField(
+        widget=forms.DateInput(format="%Y-%m-%d", attrs={"type": "date"}),
+        input_formats=["%Y-%m-%d"],
+        required=False,
+    )
+
+    TRUE_FALSE_CHOICES = ((False, "No"), (True, "Yes"))
+    is_done = forms.ChoiceField(
+        choices=TRUE_FALSE_CHOICES,
+        label="Is done ?",
+        initial="",
+        widget=forms.Select(), required=True)
+
     class Meta:
         model = Task
         fields = "__all__"
+
+    def clean_deadline(self):
+        return validate_deadline(self.cleaned_data["deadline"])
 
 
 class TagForm(forms.ModelForm):
